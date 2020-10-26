2582b86e713be0989b2e7713653d5355d4a935e2
Divyansh Bajpai <divyansh.bajpai@sap.com>
2020-10-27 04:08:50 +0530

    WIP

--- github-test.ts
+++ github-test.ts
@@ -1,3 +1,3 @@
 import {Github} from './github.ts';
 
-const gh=new Github("https://api.githib.com","divyanshbajpai","d48addbf0456ba7c15e492330fc27d4797186839")
+const gh=new Github("https://api.githib.com","user","token")
